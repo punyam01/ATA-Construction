663c06941d506a93380e63ae6290594b31fdab6f
punyam01 <punyamkumarsingh@gmail.com>
2025-04-19 14:09:26 +0530

    revised framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+'use client';
 import HeroSlider from "@/components/hero-slider"
+
 import ServicesSection from "@/components/services-section"
 import WhyChooseUsSection from "@/components/why-choose-us-section"
 import AchievementsSection from "@/components/achievements-section"
 import ProjectsSection from "@/components/projects-section"
 import ClientsSection from "@/components/clients-section"
 import TestimonialsSection from "@/components/testimonials-section"
+import { motion } from 'framer-motion';
+
 
 export default function Home() {
   return (
