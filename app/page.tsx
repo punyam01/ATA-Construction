786e7154781d92d6ba2bbf61e244a03c808ff5b5
Ayush Raj <163673624+bhardwajayush8210@users.noreply.github.com>
2025-05-09 23:57:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import WhyChooseUsSection from "@/components/why-choose-us-section"
 import AchievementsSection from "@/components/achievements-section"
 // import ProjectsSection from "@/components/projects-section"
 import ClientsSection from "@/components/clients-section"
-import TestimonialsSection from "@/components/testimonials-section"
+// import TestimonialsSection from "@/components/testimonials-section"
 import { motion } from 'framer-motion';
 
 
@@ -21,7 +21,7 @@ export default function Home() {
       <AchievementsSection />
 {/*       <ProjectsSection /> */}
       <ClientsSection />
-      <TestimonialsSection />
+{/*       <TestimonialsSection /> */}
     </>
   )
 }
