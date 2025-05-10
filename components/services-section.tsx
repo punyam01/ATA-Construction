fd8e2786ad483b95ea0d32c044ca265a349ce350
Ayush Raj <163673624+bhardwajayush8210@users.noreply.github.com>
2025-05-10 10:28:06 +0530

    Update services-section.tsx

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -17,7 +17,7 @@ const homeServices = [
     title: "Internal Finishing",
     description:
       "Our finishing services ensure high-quality interior and exterior aesthetics for all types of buildings.",
-    image: "/images/Taj Resort_04.png",
+    image: "/images/Taj Resort _04.png",
   },
  {
     id: "external-development",
